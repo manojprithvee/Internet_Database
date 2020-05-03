fcf1f4086697d518cfaff95ad18d3bb5f387a9ed
manoj <manoj.prithvee@gmail.com>
2020-05-03 19:30:25 +0530

    added Web UI

---
+++ web.py
@@ -0,0 +1,45 @@
+import os
+
+import moz_sql_parser
+from flask import Flask, render_template
+from flask import request, jsonify
+from moz_sql_parser import parse
+from pyspark import SparkContext
+
+from tables.tables import *
+
+sc = SparkContext()
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Python Flask SparkPi server running. Add the 'sparkpi' route to this URL to invoke the app."
+
+
+@app.route("/console")
+def console():
+    return render_template("console.html")
+    # return "Python Flask SparkPi server running. Add the 'sparkpi' route to this URL to invoke the app."
+
+
+@app.route("/api")
+def sparkpi():
+    sql = str(request.args.get('api', ""))
+    if sql == "":
+        sql = 'select "//section/div[1]/div[2]/div/div/div/div[1]/span/span","//section/div[1]/div[' \
+              '2]/div/div/div/h1/text()" from html where url in (select "/html/body/div[6]/main/div[3]/section/div[' \
+              '1]/div/div/div/div/div/a/@href" from html where url in ("https://www.lttstore.com") and ' \
+              'selector=xpath) and selector=xpath '
+    try:
+        parsed_sql = parse(sql)
+        table = tableclass(parsed_sql, sc)
+        response = table.run(sc)
+    except moz_sql_parser.ParseException:
+        print(moz_sql_parser.ParseException)
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
